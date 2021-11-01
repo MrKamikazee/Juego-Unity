1c37da6327e72201518181d9965f9967c371a6d9
MrKamikazee <flacomartin001@gmail.com>
2021-11-01 16:41:47 -0300

    paztero craneate esto xD

--- Assets/Scripts/Personaje/MoverPersonaje.cs
+++ Assets/Scripts/Personaje/MoverPersonaje.cs
@@ -6,38 +6,58 @@ using UnityEngine;
 public class MoverPersonaje : MonoBehaviour
 {
     public Rigidbody2D rb;
-    [Header("Move Mechanics")]
-    public Transform personajeTransform;
+    [Header("Move Mechanics")] public Transform personajeTransform;
     public float speedPersonaje;
 
-    [Header("Jump Mechanics")] 
-    [Range(1, 20)]
+    [Header("Jump Mechanics")] [Range(1, 20)]
     public float strenghJump;
+
     public float fallMultipler = 2.5f;
     public float lowJumpMultipler = 2;
     public bool isGrounded = true;
-    
-    void Update()
+    public float doubleJumpSpeed = 2.5f;
+    public bool canDoubleJump;
+
+    private void Update()
     {
         //Mueve el personaje de izquierda a derecha
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             personajeTransform.Translate((Vector2.right * Time.deltaTime * speedPersonaje));
         }
+
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             personajeTransform.Translate((Vector2.left * Time.deltaTime * speedPersonaje));
         }
-        
-        //Permite al personaje saltar
-        if (Input.GetButtonDown("Jump") && isGrounded)
+
+        // salto normal y doble salto
+        if (Input.GetButtonDown("Jump"))
         {
-            Jump();
+            if (isGrounded)
+            {
+                canDoubleJump = true;
+                Jump();
+            }
+            else
+            {
+                if (Input.GetButtonDown("Jump"))
+                {
+                    if (canDoubleJump)
+                    {
+                        //rb.velocity = Vector2.up * doubleJumpSpeed;
+                        canDoubleJump = false;
+                        Jump();
+                    }
+                }
+            }
         }
+
         if (!isGrounded && (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)))
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultipler + 1) * Time.deltaTime;
         }
+
         if (rb.velocity.y < 0)
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultipler - 1) * Time.deltaTime;
@@ -50,14 +70,16 @@ public class MoverPersonaje : MonoBehaviour
 
     private void Jump()
     {
-        rb.velocity = Vector2.up * strenghJump; 
+        rb.velocity = Vector2.up * strenghJump;
     }
+
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Floor") || other.CompareTag("PlataformaMovil"))
         {
             isGrounded = true;
         }
+
         if (other.CompareTag("PlataformaMovil"))
         {
             transform.parent = other.transform; //Arreglar linea de codigo
@@ -76,6 +98,4 @@ public class MoverPersonaje : MonoBehaviour
             transform.parent = null;
         }
     }
-
-    
 }
