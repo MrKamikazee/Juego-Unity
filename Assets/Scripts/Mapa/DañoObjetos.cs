fcb73c46ffcc7f2a5db63123b35a081a240e9a1a
Paztero <matiaspino410@gmail.com>
2021-11-28 18:26:11 -0300

    Sonidos y weas

--- Assets/Scripts/Mapa/DañoObjetos.cs
+++ Assets/Scripts/Mapa/DañoObjetos.cs
@@ -2,15 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Audio;
 
 public class DañoObjetos : MonoBehaviour
 {
+    public AudioSource clipDaño;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.transform.CompareTag("Player"))
         {
             other.transform.GetComponent<RespawnPersonaje>().PlayerDamaged();
             other.transform.GetComponent<RespawnPersonaje>().TPCheckPoint();
+            clipDaño.Play();
         }
     }
 }
