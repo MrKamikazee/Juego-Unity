e35d87f4fef9f19e52164320ddd679bedb174def
MrKamikazee <flacomartin001@gmail.com>
2021-11-30 20:32:30 -0300

    asd

--- Assets/Scripts/Mapa/HistoriaInicial.cs
+++ Assets/Scripts/Mapa/HistoriaInicial.cs
@@ -17,5 +17,7 @@ public class HistoriaInicial : MonoBehaviour
         yield return new WaitForSeconds(10);
         panelHistoria.DOFade(0, 0.5f);
         paredesInvicibles.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        panelHistoria.gameObject.SetActive(false);
     }
 }
